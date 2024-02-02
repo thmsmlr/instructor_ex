5aff93605051fa46665989916f030eb2548aa1db
Andres Alejos <andres.c.alejos.mil@army.mil>
2024-02-02 17:20:59 -0500

    Pass OpenAI config as param

--- lib/instructor/adapters/openai.ex
+++ lib/instructor/adapters/openai.ex
@@ -5,13 +5,14 @@ defmodule Instructor.Adapters.OpenAI do
   @behaviour Instructor.Adapter
 
   @impl true
-  def chat_completion(params, config \\ %OpenAI.Config{}) do
+  def chat_completion(params) do
     # Peel off instructor only parameters
     # TODO: Maybe refactor this? we'll see
     {_, params} = Keyword.pop(params, :response_model)
     {_, params} = Keyword.pop(params, :validation_context)
     {_, params} = Keyword.pop(params, :max_retries)
     {_, params} = Keyword.pop(params, :mode)
+    {config, params} = Keyword.pop(params, :config, %OpenAI.Config{})
 
     OpenAI.chat_completion(params, config)
   end
