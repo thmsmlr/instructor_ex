a5d8b3f8a5989e4e2bcfa0da40a7132392d93705
Thomas Millar <millar.thomas@gmail.com>
2024-02-15 11:19:58 -0600

    Remove dependency on openai client, smart defaults
    I'm removing the dependency on the openai client, we only use it for a
    single openai endpoint. It makes configuration a little weird because
    not all configurations can be customized at runtime. I don't want you to
    have to configure openai client globally incase you're using it
    elsewhere with different constraints. This allows me to change the
    defaults to enable streaming which leads to a much simpler install
    experience.
    
    This also updates the tests and the adapter protocol to properly plumb
    the config through the instructor library as it should've been.

--- lib/instructor/adapters/openai.ex
+++ lib/instructor/adapters/openai.ex
@@ -5,14 +5,99 @@ defmodule Instructor.Adapters.OpenAI do
   @behaviour Instructor.Adapter
 
   @impl true
-  def chat_completion(params, config \\ %OpenAI.Config{}) do
+  def chat_completion(params, config) do
+    config = if config, do: config, else: config()
+
     # Peel off instructor only parameters
-    # TODO: Maybe refactor this? we'll see
     {_, params} = Keyword.pop(params, :response_model)
     {_, params} = Keyword.pop(params, :validation_context)
     {_, params} = Keyword.pop(params, :max_retries)
     {_, params} = Keyword.pop(params, :mode)
+    stream = Keyword.get(params, :stream, false)
+    params = Enum.into(params, %{})
 
-    OpenAI.chat_completion(params, config)
+    if stream do
+      do_streaming_chat_completion(params, config)
+    else
+      do_chat_completion(params, config)
+    end
+  end
+
+  defp do_streaming_chat_completion(params, config) do
+    pid = self()
+    options = http_options(config)
+
+    Stream.resource(
+      fn ->
+        Task.async(fn ->
+          options =
+            Keyword.merge(options,
+              json: params,
+              auth: {:bearer, api_key(config)},
+              into: fn {:data, data}, {req, resp} ->
+                chunks =
+                  data
+                  |> String.split("\n")
+                  |> Enum.filter(fn line ->
+                    String.starts_with?(line, "data: {")
+                  end)
+                  |> Enum.map(fn line ->
+                    line
+                    |> String.replace_prefix("data: ", "")
+                    |> Jason.decode!()
+                  end)
+
+                for chunk <- chunks do
+                  send(pid, chunk)
+                end
+
+                {:cont, {req, resp}}
+              end
+            )
+
+          Req.post!(url(config), options)
+          send(pid, :done)
+        end)
+      end,
+      fn task ->
+        receive do
+          :done ->
+            {:halt, task}
+
+          data ->
+            {[data], task}
+        after
+          15_000 ->
+            {:halt, task}
+        end
+      end,
+      fn task -> Task.await(task) end
+    )
+  end
+
+  defp do_chat_completion(params, config) do
+    options = Keyword.merge(http_options(config), json: params, auth: {:bearer, api_key(config)})
+    response = Req.post!(url(config), options)
+
+    case response.status do
+      200 -> {:ok, response.body}
+      _ -> {:error, response.body}
+    end
+  end
+
+  defp url(config), do: api_url(config) <> "/v1/chat/completions"
+  defp api_url(config), do: Keyword.fetch!(config, :api_url)
+  defp api_key(config), do: Keyword.fetch!(config, :api_key)
+  defp http_options(config), do: Keyword.fetch!(config, :http_options)
+
+  defp config() do
+    base_config = Application.get_env(:instructor, :openai, [])
+
+    default_config = [
+      api_url: "https://api.openai.com",
+      http_options: [receive_timeout: 60_000]
+    ]
+
+    Keyword.merge(default_config, base_config)
   end
 end
