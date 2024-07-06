3b8f02fce5452c32f6cc0c1787270181dfb8281d
martosaur <martosaur@gmail.com>
2024-07-06 13:37:09 -0700

    Introduce Instructor.prepare_prompt/2 and Instructor.consume_response/2

--- lib/instructor/adapter.ex
+++ lib/instructor/adapter.ex
@@ -2,5 +2,6 @@ defmodule Instructor.Adapter do
   @moduledoc """
   Behavior for `Instructor.Adapter`.
   """
-  @callback chat_completion([Keyword.t()], any()) :: any()
+  @callback chat_completion(map(), [Keyword.t()], any()) :: any()
+  @callback prompt(Keyword.t()) :: map()
 end
--- test/support/test_helpers.ex
+++ test/support/test_helpers.ex
@@ -1,75 +1,73 @@
 defmodule Instructor.TestHelpers do
   import Mox
 
-  def mock_openai_response(:tools, result) do
-    InstructorTest.MockOpenAI
-    |> expect(:chat_completion, fn _params, _config ->
-      {:ok,
-       %{
-         "id" => "chatcmpl-8e9AVo9NHfvBG5cdtAEiJMm7q4Htz",
-         "usage" => %{
-           "completion_tokens" => 23,
-           "prompt_tokens" => 136,
-           "total_tokens" => 159
-         },
-         "choices" => [
-           %{
-             "finish_reason" => "stop",
-             "index" => 0,
-             "logprobs" => nil,
-             "message" => %{
-               "content" => nil,
-               "role" => "assistant",
-               "tool_calls" => [
-                 %{
-                   "function" => %{
-                     "arguments" => Jason.encode!(result),
-                     "name" => "schema"
-                   },
-                   "id" => "call_DT9fBvVCHWGSf9IeFZnlarIY",
-                   "type" => "function"
-                 }
-               ]
-             }
-           }
-         ],
-         "model" => "gpt-3.5-turbo-0613",
-         "object" => "chat.completion",
-         "created" => 1_704_579_055,
-         "system_fingerprint" => nil
-       }}
-    end)
+  def example_openai_response(:tools, result) do
+    %{
+      "id" => "chatcmpl-8e9AVo9NHfvBG5cdtAEiJMm7q4Htz",
+      "usage" => %{
+        "completion_tokens" => 23,
+        "prompt_tokens" => 136,
+        "total_tokens" => 159
+      },
+      "choices" => [
+        %{
+          "finish_reason" => "stop",
+          "index" => 0,
+          "logprobs" => nil,
+          "message" => %{
+            "content" => nil,
+            "role" => "assistant",
+            "tool_calls" => [
+              %{
+                "function" => %{
+                  "arguments" => Jason.encode!(result),
+                  "name" => "schema"
+                },
+                "id" => "call_DT9fBvVCHWGSf9IeFZnlarIY",
+                "type" => "function"
+              }
+            ]
+          }
+        }
+      ],
+      "model" => "gpt-3.5-turbo-0613",
+      "object" => "chat.completion",
+      "created" => 1_704_579_055,
+      "system_fingerprint" => nil
+    }
   end
 
-  def mock_openai_response(mode, result) when mode in [:json, :md_json] do
-    InstructorTest.MockOpenAI
-    |> expect(:chat_completion, fn _params, _config ->
-      {
-        :ok,
+  def example_openai_response(mode, result) when mode in [:json, :md_json] do
+    %{
+      "id" => "chatcmpl-8e9AVo9NHfvBG5cdtAEiJMm7q4Htz",
+      "usage" => %{
+        "completion_tokens" => 23,
+        "prompt_tokens" => 136,
+        "total_tokens" => 159
+      },
+      "choices" => [
         %{
-          "id" => "chatcmpl-8e9AVo9NHfvBG5cdtAEiJMm7q4Htz",
-          "usage" => %{
-            "completion_tokens" => 23,
-            "prompt_tokens" => 136,
-            "total_tokens" => 159
-          },
-          "choices" => [
-            %{
-              "finish_reason" => "stop",
-              "index" => 0,
-              "logprobs" => nil,
-              "message" => %{
-                "content" => Jason.encode!(result),
-                "role" => "assistant"
-              }
-            }
-          ],
-          "model" => "gpt-3.5-turbo-0613",
-          "object" => "chat.completion",
-          "created" => 1_704_579_055,
-          "system_fingerprint" => nil
+          "finish_reason" => "stop",
+          "index" => 0,
+          "logprobs" => nil,
+          "message" => %{
+            "content" => Jason.encode!(result),
+            "role" => "assistant"
+          }
         }
-      }
+      ],
+      "model" => "gpt-3.5-turbo-0613",
+      "object" => "chat.completion",
+      "created" => 1_704_579_055,
+      "system_fingerprint" => nil
+    }
+  end
+
+  def mock_openai_response(mode, result) do
+    InstructorTest.MockOpenAI
+    |> expect(:prompt, &Instructor.Adapters.OpenAI.prompt/1)
+    |> expect(:chat_completion, fn _prompt, _params, _config ->
+      {:ok, example_openai_response(mode, result)}
     end)
   end
 
@@ -114,7 +112,8 @@ defmodule Instructor.TestHelpers do
         ]
 
     InstructorTest.MockOpenAI
-    |> expect(:chat_completion, fn _params, _config ->
+    |> expect(:prompt, &Instructor.Adapters.OpenAI.prompt/1)
+    |> expect(:chat_completion, fn _prompt, _params, _config ->
       chunks
     end)
   end
@@ -160,7 +159,8 @@ defmodule Instructor.TestHelpers do
         ]
 
     InstructorTest.MockOpenAI
-    |> expect(:chat_completion, fn _params, _config ->
+    |> expect(:prompt, &Instructor.Adapters.OpenAI.prompt/1)
+    |> expect(:chat_completion, fn _prompt, _params, _config ->
       chunks
     end)
   end
