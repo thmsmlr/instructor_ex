0a04ec51a201a82a8e1db7391b78cdc9002d821c
Andres Alejos <andres.alejos@gmail.com>
2024-10-21 15:50:14 -0400

    implement validation callback in extension

--- lib/instructor/instruction.ex
+++ lib/instructor/instruction.ex
@@ -2,4 +2,19 @@ defmodule Instructor.Instruction do
   use Flint.Extension
 
   option :doc, default: "", validator: &is_binary/1, required: false
+
+  defmacro __using__(_opts) do
+    quote do
+      use Instructor.Validator
+
+      @impl true
+      def validate_changeset(changeset, context \\ %{}) do
+        __MODULE__
+        |> struct!()
+        |> changeset(changeset, Enum.into(context, []))
+      end
+
+      defoverridable validate_changeset: 1, validate_changeset: 2
+    end
+  end
 end
