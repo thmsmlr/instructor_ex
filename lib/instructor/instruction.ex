0db5f984b71b2cc7bb052294736195357bc6a44b
Andres Alejos <andres.alejos@gmail.com>
2024-11-20 20:23:12 -0500

    add template attribute

--- lib/instructor/instruction.ex
+++ lib/instructor/instruction.ex
@@ -3,28 +3,54 @@ if Code.ensure_loaded?(Flint.Schema) do
     use Flint.Extension
 
     attribute(:stream, default: false, validator: &is_boolean/1)
-    # attribute(:validation_context, default: %{}, validator: &is_map/1)
+    attribute(:validation_context, default: %{}, validator: &is_map/1)
     attribute(:mode, default: :tools, validator: &Kernel.in(&1, [:tools, :json, :md_json]))
     attribute(:max_retries, default: 0, validator: &is_integer/1)
-    attribute(:system_prompt, validator: &is_binary/1, default: "")
-    attribute(:model, validator: &is_binary/1, default: "")
+    attribute(:system_prompt, validator: &is_binary/1)
+    attribute(:model, validator: &is_binary/1)
     attribute(:array, default: false, validator: &is_boolean/1)
+    attribute(:template)
 
     option(:doc, default: "", validator: &is_binary/1, required: false)
 
     defmacro __using__(_opts) do
       quote do
         use Instructor.Validator
+        require EEx
+
+        def render_template(assigns) do
+          EEx.eval_string(__MODULE__.__schema__(:template), assigns: assigns)
+        end
 
         def chat_completion(messages, opts \\ []) do
           opts =
             Keyword.validate!(opts,
               stream: __MODULE__.__schema__(:stream),
-              # validation_context: __MODULE__.__schema__(:validation_context,
+              validation_context: __MODULE__.__schema__(:validation_context),
               mode: __MODULE__.__schema__(:mode),
               max_retries: __MODULE__.__schema__(:max_retries),
               model: __MODULE__.__schema__(:model)
             )
+
+          messages = if Keyword.keyword?(messages), do: [messages], else: messages
+
+          messages =
+            for message <- messages do
+              case message do
+                %{role: _role, content: _content} ->
+                  message
+
+                _ ->
+                  %{
+                    role: "user",
+                    content:
+                      if(__MODULE__.__schema__(:template),
+                        do: render_template(message),
+                        else: message
+                      )
+                  }
+              end
+            end
 
           messages =
             if __MODULE__.__schema__(:system_prompt) do
@@ -36,7 +62,7 @@ if Code.ensure_loaded?(Flint.Schema) do
           response_model =
             if __MODULE__.__schema__(:array), do: {:array, __MODULE__}, else: __MODULE__
 
-          opts = ([messages: messages, response_model: response_model] ++ opts) |> IO.inspect()
+          opts = ([messages: messages, response_model: response_model] ++ opts)
           Instructor.chat_completion(opts)
         end
 
