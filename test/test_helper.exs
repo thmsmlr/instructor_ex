63129d29c6d09226301ce332b47eb212478d3d06
Thomas Millar <millar.thomas@gmail.com>
2025-02-08 22:41:45 -0600

    docs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -5,6 +5,11 @@ Mox.defmock(InstructorTest.MockOpenAI, for: Instructor.Adapter)
 #   mix test --only adapter:llamacpp
 #   mix test --only adapter:openai
 #
+# to run all the non-local models, use:
+#
+#   mix test --include adapter:gemini --include adapter:anthropic --include adapter:openai
+#
+#
 ExUnit.configure(
   exclude: [
     adapter: :openai,
